f81037b6a8e05a17ebd7976c7cf4fa3a9915cdbc
Jonathan Archer <jon@jonnyarcher.com>
2019-01-29 22:35:01 -0600

    Ensure multiboot header gets linked to the beginning.

--- kernel/src/boot/boot.s
+++ kernel/src/boot/boot.s
@@ -2,6 +2,8 @@
 ; boot.s -- Kernel start location. Also defines multiboot header.
 ;
 
+section .multiboot
+
 MBOOT_PAGE_ALIGN    equ 1<<0    ; Load kernel and modules on a page boundary
 MBOOT_MEM_INFO      equ 1<<1    ; Provide your kernel with memory info
 MBOOT_HEADER_MAGIC  equ 0x1BADB002 ; Multiboot Magic value
@@ -9,7 +11,6 @@ MBOOT_HEADER_MAGIC  equ 0x1BADB002 ; Multiboot Magic value
 ; pass us a symbol table.
 MBOOT_HEADER_FLAGS  equ MBOOT_PAGE_ALIGN | MBOOT_MEM_INFO
 MBOOT_CHECKSUM      equ -(MBOOT_HEADER_MAGIC + MBOOT_HEADER_FLAGS)
-
 
 [BITS 32]                       ; All instructions should be 32-bit.
 
