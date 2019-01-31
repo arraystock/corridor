c90610e489466da341ba8eb9e19539fe457ccbcd
Jonathan Archer <jon@jonnyarcher.com>
2019-01-31 09:10:27 -0600

    Small changes to comments.

--- kernel/src/boot/boot.s
+++ kernel/src/boot/boot.s
@@ -8,9 +8,9 @@ extern k_main
 section .multiboot
 
 align 4
-dd 0x1BADB002                   ; Magic
-dd 0x00                         ; Flags
-dd - (0x1BADB002 + 0x00)        ; Checksum
+dd 0x1BADB002                   ; Magic.
+dd 0x00                         ; Flags.
+dd - (0x1BADB002 + 0x00)        ; Checksum.
 
 section .text
 
@@ -19,12 +19,12 @@ start:
   cli                   ; Disable interrupts.
   mov esp, stack        ; Set up the stack.
   call enable_paging    ; Enable paging and remap kernel to 3GB.
-  xor ebp, ebp          ; Prepare NULL stack frame so traces know where to stop
+  xor ebp, ebp          ; Prepare NULL stack frame so traces know where to stop.
   push ebp
-  push ebx              ; Load multiboot header location
+  push ebx              ; Load multiboot header location.
   call k_main           ; Call k_main().
   jmp $                 ; Loop forever.
 
 section .bss
-resb 8192               ; Measured in bytes, 8KB
+resb 8192               ; Measured in bytes, 8KB.
 stack:
--- kernel/src/boot/paging.s
+++ kernel/src/boot/paging.s
@@ -6,40 +6,40 @@
 
 global enable_paging
 enable_paging:
-  pusha                   ; save stack frame
+  pusha                         ; Save stack frame.
 
   ;------------------------------------------
-  ; idenitity map 1st page table (4MB)
+  ; Idenitity map 1st page table (4MB).
   ;------------------------------------------
 
-  mov   eax, PAGE_TABLE_0       ; first page table
-  mov   ebx, 0x0 | PRIV         ; starting physical address of page
-  mov   ecx, PAGE_TABLE_ENTRIES ; for every page in table...
+  mov   eax, PAGE_TABLE_0       ; First page table.
+  mov   ebx, 0x0 | PRIV         ; Starting physical address of page.
+  mov   ecx, PAGE_TABLE_ENTRIES ; For every page in table...
 .loop:
-  mov   dword [eax], ebx        ; write the entry
-  add   eax, 4                  ; go to next page entry in table (Each entry is 4 bytes)
-  add   ebx, 4096               ; go to next page address (Each page is 4Kb)
-  loop  .loop                   ; go to next entry
+  mov   dword [eax], ebx        ; Write the entry.
+  add   eax, 4                  ; Go to next page entry in table (Each are 4 bytes).
+  add   ebx, 4096               ; Go to next page address (Each page is 4Kb).
+  loop  .loop                   ; Go to next entry.
 
   ;------------------------------------------
-  ; set up the entries in the directory table
+  ; Set up entries in the directory table.
   ;------------------------------------------
 
-  mov   eax, PAGE_TABLE_0 | PRIV      ; 1st table is directory entry 0
+  mov   eax, PAGE_TABLE_0 | PRIV        ; 1st table is directory entry 0.
   mov   dword [PAGE_DIR], eax
 
-  mov   eax, PAGE_TABLE_768 | PRIV      ; 768th entry in directory table
+  mov   eax, PAGE_TABLE_768 | PRIV      ; 768th entry in directory table.
   mov   dword [PAGE_DIR+(768*4)], eax
 
   ;------------------------------------------
-  ; install directory table
+  ; Install the directory table.
   ;------------------------------------------
 
   mov   eax, PAGE_DIR
   mov   cr3, eax
 
   ;------------------------------------------
-  ; enable paging
+  ; Enable paging.
   ;------------------------------------------
 
   mov   eax, cr0
@@ -47,18 +47,18 @@ enable_paging:
   mov   cr0, eax
 
   ;------------------------------------------
-  ; map the 768th table to physical addr 1MB
-  ; the 768th table starts the 3gb virtual address
+  ; Map the 768th table to physical addr 1MB.
+  ; 768th table starts the 3GB virtual addr.
   ;------------------------------------------
 
-  mov   eax, PAGE_TABLE_768       ; first page table
-  mov   ebx, 0x100000 | PRIV      ; starting physical address of page
-  mov   ecx, PAGE_TABLE_ENTRIES     ; for every page in table...
+  mov   eax, PAGE_TABLE_768     ; First page table.
+  mov   ebx, 0x100000 | PRIV    ; Starting physical address of page.
+  mov   ecx, PAGE_TABLE_ENTRIES ; For every page in table...
 .loop2:
-  mov   dword [eax], ebx        ; write the entry
-  add   eax, 4              ; go to next page entry in table (Each entry is 4 bytes)
-  add   ebx, 4096           ; go to next page address (Each page is 4Kb)
-  loop  .loop2              ; go to next entry
+  mov   dword [eax], ebx        ; Write the entry.
+  add   eax, 4                  ; Go to next page entry in table (Each are 4 bytes).
+  add   ebx, 4096               ; Go to next page address (Each page is 4Kb).
+  loop  .loop2                  ; Go to next entry.
 
   popa
   ret
