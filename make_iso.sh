16655c566fb6bb530e65c6f0e1ace5876ae429bc
Jonathan Archer <jon@jonnyarcher.com>
2019-01-30 21:43:44 -0600

    Fix/Improve ISO generation.

--- make_iso.sh
+++ make_iso.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 
+if (($# != 3)); then
+	echo "Usage: config kernel iso"
+	exit 1
+fi
+
 mkdir -p iso/boot/grub
-cp grub.cfg iso/boot/grub/
-cp kernel/bin/* iso/boot/
-grub-mkrescue -o corridor.iso iso
+cp $1 iso/boot/grub/
+cp $2 iso/boot/
+grub-mkrescue -o $3 iso
