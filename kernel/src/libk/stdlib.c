0ae07cf9065c9f27465b29f40aa6c854c6fe17cb
Jonathan Archer <jon@jonnyarcher.com>
2019-02-03 17:18:56 -0600

    Simplify panic screen.

--- kernel/src/libk/stdlib.c
+++ kernel/src/libk/stdlib.c
@@ -116,53 +116,37 @@ void abort() {
 }
 
 void kpanic(char *err, struct regs *r) {
-  monitor_set_bg_color(BLUE);
-  monitor_clear();
-
   monitor_set_fg_color(YELLOW);
-  monitor_set_bg_color(LBLUE);
 
-  for (int i = 0; i < 80; i++)
-    putchar(' ');
-
-  monitor_write_center("KERNEL PANIC");
-
-  for (int i = 0; i < 80; i++)
-    putchar(' ');
-
-  monitor_set_fg_color(YELLOW);
-  monitor_set_bg_color(BLUE);
-
-  monitor_write("\n");
-  monitor_write_center(err);
-  monitor_write("\n");
+  printf("KERNEL PANIC: %s\n\n", err);
 
   if (r != 0) {
+    monitor_set_fg_color(WHITE);
     monitor_write("[");
     monitor_set_fg_color(LGRAY);
     monitor_write("EAX: ");
-    monitor_set_fg_color(YELLOW);
+    monitor_set_fg_color(WHITE);
     monitor_write_hex(r->eax);
     monitor_write("] ");
 
     monitor_write("[");
     monitor_set_fg_color(LGRAY);
     monitor_write("EBX: ");
-    monitor_set_fg_color(YELLOW);
+    monitor_set_fg_color(WHITE);
     monitor_write_hex(r->ebx);
     monitor_write("] ");
 
     monitor_write("[");
     monitor_set_fg_color(LGRAY);
     monitor_write("ECX: ");
-    monitor_set_fg_color(YELLOW);
+    monitor_set_fg_color(WHITE);
     monitor_write_hex(r->ecx);
     monitor_write("] ");
 
     monitor_write("[");
     monitor_set_fg_color(LGRAY);
     monitor_write("EDX: ");
-    monitor_set_fg_color(YELLOW);
+    monitor_set_fg_color(WHITE);
     monitor_write_hex(r->edx);
     monitor_write("] ");
 
@@ -171,28 +155,28 @@ void kpanic(char *err, struct regs *r) {
     monitor_write("[");
     monitor_set_fg_color(LGRAY);
     monitor_write("EIP: ");
-    monitor_set_fg_color(YELLOW);
+    monitor_set_fg_color(WHITE);
     monitor_write_hex(r->eip);
     monitor_write("] ");
 
     monitor_write("[");
     monitor_set_fg_color(LGRAY);
     monitor_write("EBP: ");
-    monitor_set_fg_color(YELLOW);
+    monitor_set_fg_color(WHITE);
     monitor_write_hex(r->ebp);
     monitor_write("] ");
 
     monitor_write("[");
     monitor_set_fg_color(LGRAY);
     monitor_write("ESP: ");
-    monitor_set_fg_color(YELLOW);
+    monitor_set_fg_color(WHITE);
     monitor_write_hex(r->esp);
     monitor_write("] ");
 
     monitor_write("[");
     monitor_set_fg_color(LGRAY);
     monitor_write("ESI: ");
-    monitor_set_fg_color(YELLOW);
+    monitor_set_fg_color(WHITE);
     monitor_write_hex(r->esi);
     monitor_write("] ");
 
@@ -201,28 +185,28 @@ void kpanic(char *err, struct regs *r) {
     monitor_write("[");
     monitor_set_fg_color(LGRAY);
     monitor_write("EDI: ");
-    monitor_set_fg_color(YELLOW);
+    monitor_set_fg_color(WHITE);
     monitor_write_hex(r->edi);
     monitor_write("] ");
 
     monitor_write("[");
     monitor_set_fg_color(LGRAY);
     monitor_write("DS:  ");
-    monitor_set_fg_color(YELLOW);
+    monitor_set_fg_color(WHITE);
     monitor_write_hex(r->ds);
     monitor_write("] ");
 
     monitor_write("[");
     monitor_set_fg_color(LGRAY);
     monitor_write("ES:  ");
-    monitor_set_fg_color(YELLOW);
+    monitor_set_fg_color(WHITE);
     monitor_write_hex(r->es);
     monitor_write("] ");
 
     monitor_write("[");
     monitor_set_fg_color(LGRAY);
     monitor_write("GS:  ");
-    monitor_set_fg_color(YELLOW);
+    monitor_set_fg_color(WHITE);
     monitor_write_hex(r->gs);
     monitor_write("] ");
 
@@ -231,28 +215,28 @@ void kpanic(char *err, struct regs *r) {
     monitor_write("[");
     monitor_set_fg_color(LGRAY);
     monitor_write("FS:  ");
-    monitor_set_fg_color(YELLOW);
+    monitor_set_fg_color(WHITE);
     monitor_write_hex(r->fs);
     monitor_write("] ");
 
     monitor_write("[");
     monitor_set_fg_color(LGRAY);
     monitor_write("CS:  ");
-    monitor_set_fg_color(YELLOW);
+    monitor_set_fg_color(WHITE);
     monitor_write_hex(r->cs);
     monitor_write("] ");
 
     monitor_write("[");
     monitor_set_fg_color(LGRAY);
     monitor_write("SS:  ");
-    monitor_set_fg_color(YELLOW);
+    monitor_set_fg_color(WHITE);
     monitor_write_hex(r->ss);
     monitor_write("] ");
 
     monitor_write("[");
     monitor_set_fg_color(LGRAY);
     monitor_write("ERR: ");
-    monitor_set_fg_color(YELLOW);
+    monitor_set_fg_color(WHITE);
     monitor_write_hex(r->err_code);
     monitor_write("] ");
 
@@ -262,45 +246,40 @@ void kpanic(char *err, struct regs *r) {
     monitor_write("[");
     monitor_set_fg_color(LGRAY);
     monitor_write("EFLAGS: ");
-    monitor_set_fg_color(YELLOW);
+    monitor_set_fg_color(WHITE);
     monitor_write_hex(r->eflags);
     monitor_write("] ");
 
     monitor_write("[");
     monitor_set_fg_color(LGRAY);
     monitor_write("USRESP: ");
-    monitor_set_fg_color(YELLOW);
+    monitor_set_fg_color(WHITE);
     monitor_write_hex(r->useresp);
     monitor_write("] ");
 
     monitor_write("[");
     monitor_set_fg_color(LGRAY);
     monitor_write("INTNO: ");
-    monitor_set_fg_color(YELLOW);
+    monitor_set_fg_color(WHITE);
     monitor_write_hex(r->int_no);
     monitor_write("] ");
 
     monitor_write("\n");
   }
 
-  monitor_write("\n\n");
+  monitor_write("\n");
   /*
   monitor_write_center("Dedicated to G,");
   monitor_write_center("who helps me fix all the errors in my life.");
   */
-  monitor_write_center("The kernel done goofed!");
-  monitor_write("\n");
-  monitor_write_center(
-      "We're still in development, so this isn't completely unexpected.");
+  printf("The kernel done goofed!\n");
+  printf("We're still in development, so this isn't completely unexpected.\n");
 
-  monitor_write("\n");
-
-  monitor_write_center("Please start an issue and submit a screenshot.");
-  monitor_write_center(
-      "You can do this at 'https://github.com/arraystock/corridor'.");
-  monitor_write("\n");
-  monitor_write_center(" Thank "
-                       "you for using corridor!");
+  printf("Please take a screenshot and start an issue at:\n");
+  monitor_set_fg_color(LGRAY);
+  printf("https://github.com/arraystock/corridor");
+  monitor_set_fg_color(WHITE);
+  printf("\nThank you for using corridor!\n");
 
   abort();
 }
