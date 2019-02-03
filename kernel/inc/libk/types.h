3efcfa5ffd830a436986aa0715ce138861d7129f
Jonathan Archer <jon@jonnyarcher.com>
2019-02-03 16:58:13 -0600

    Update types.h

--- kernel/inc/libk/types.h
+++ kernel/inc/libk/types.h
@@ -1,11 +1,13 @@
 #ifndef TYPES_H
 #define TYPES_H
 
+typedef unsigned long long uint64_t;
+typedef long long int64_t;
 typedef unsigned int uint32_t;
-typedef int sint32_t;
+typedef int int32_t;
 typedef unsigned short uint16_t;
-typedef short sint16_t;
+typedef short int16_t;
 typedef unsigned char uint8_t;
-typedef char sint8_t;
+typedef char int8_t;
 
 #endif
